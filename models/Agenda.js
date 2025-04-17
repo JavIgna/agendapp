9ed6944dae26006e823031c15f08611d67100a6c
JesusRojasMartinez <jesus.abr.rojas@gmail.com>
2025-04-17 15:32:15 -0400

    Agrega esquema Agenda

--- models/Agenda.js
+++ models/Agenda.js
@@ -1,15 +1,48 @@
-/*
+import mongoose from "mongoose";
+import Doctor from "Doctor.js";
+import Paciente from "Paciente.js";
+
+const esquemaAgendaMedica = new mongoose.Schema(
+  {
+
 Agenda: {
-  doctor,
-  fecha,
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+    required: true,
+    unique: true,
+  },
+  fecha : 
+  {
+    type:Date,
+     require:true
+  },
   bloques:[
     {
-      hora,
-      agendado,
-      paciente
+      hora : 
+      {
+        type:Date,
+         require:true 
+      },
+      agendado : {
+        type:String,
+        enum :["Agendado","Disponible"],
+        required:true,
+      },
+      paciente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Paciente",
+        required: true,
+        unique: true,
+      },
     }
   ]
 }
-*/
 
-// Idea de modelo
+
+},
+{ timestamps: true }
+);
+
+export const AgendaMedica = mongoose.model("AgendaMédica", esquemaAgendaMedica);
+
